204830fbb739c99ad03c8745c96c35504f3d6ac1
Veysel Üstüntaş <veyselustuntas1@gmail.com>
2023-12-14 22:55:52 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-kullanici_adi="vustuntas"
-echo "Merhaba ben $kullanici_adi." >> ~/ben.txt
+kullanici_adi="veysel"
+kullanici_soyadi="ustuntas"
+tarih=$date
+okul_no=200707048
+echo "Merhaba ben $kullanici_adi $kullanici_soyadi." >> ~/ben.txt
+echo "Okul Numaram $okul_no" >> ~/ben.txt
 echo "Bugünün Tarihi: $(date)" >> ~/ben.txt
+cat ~/ben.txt

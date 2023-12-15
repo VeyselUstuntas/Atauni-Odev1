673f0b68b15e12b1464523fc5b7c462d57ad5d19
Veysel Üstüntaş <70425868+VeyselUstuntas@users.noreply.github.com>
2023-12-15 20:02:00 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ kullanici_adi="veysel"
 kullanici_soyadi="ustuntas"
 tarih=$date
 okul_no=200707048
-echo "Merhaba ben $kullanici_adi $kullanici_soyadi." >> ~/ben.txt
-echo "Okul Numaram $okul_no" >> ~/ben.txt
-echo "Bugünün Tarihi: $(date)" >> ~/ben.txt
-cat ~/ben.txt
+echo "Merhaba ben $kullanici_adi $kullanici_soyadi." >> ./ben.txt
+echo "Okul Numaram $okul_no" >> ./ben.txt
+echo "Bugünün Tarihi: $(date)" >> ./ben.txt
+cat ./ben.txt
